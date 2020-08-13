71e45f49ff6895c25697ae95fd786ea9b93fcd8d
Pavel <wabloxa@yandex.ru>
2020-08-13 10:33:56 +0300

    Remove wait

--- run-tests-on-ci.sh
+++ run-tests-on-ci.sh
@@ -2,6 +2,4 @@ export HOSTNAME=localhost
 export PORT1=1337
 export PORT2=1338
 npx testcafe remote test/test.js --hostname ${HOSTNAME} --ports ${PORT1},${PORT2} &
-export PID=$!
 open -a Safari http://${HOSTNAME}:${PORT1}/browser/connect
-wait $PID
