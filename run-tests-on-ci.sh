8c4c546f898f1c3a7f9bb6f3be2ccfc289817d67
Pavel <wabloxa@yandex.ru>
2020-08-13 10:44:38 +0300

    Fix wait

--- run-tests-on-ci.sh
+++ run-tests-on-ci.sh
@@ -2,4 +2,6 @@ export HOSTNAME=localhost
 export PORT1=1337
 export PORT2=1338
 npx testcafe remote test/test.js --hostname ${HOSTNAME} --ports ${PORT1},${PORT2} &
+pid=$!
 open -a Safari http://${HOSTNAME}:${PORT1}/browser/connect
+wait $pid
